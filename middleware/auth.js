281213580cfa1066282556631677a51b6e2af721
dhirajdkv <dhiraj9566@gmail.com>
2024-05-17 23:14:11 -0700

    Updates APIs

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,28 +1,26 @@
 const ErrorHandler = require("../utils/errorhandler");
 const catchAsyncErrors = require("./catchAsyncErrors");
-const jwt = require('jsonwebtoken')
-const User = require('../models/userModel')
+const jwt = require("jsonwebtoken");
+const User = require("../models/userModel");
 
 exports.isAuthenticatedUser = catchAsyncErrors(async (req, res, next) => {
-    const token = req.cookies.token;
-    if (!token) {
-        return next(new ErrorHandler("Please login to access this resource", 401))
-    }
+  //   const token = req.cookies.token;
+  //   if (!token) {
+  //     return next(new ErrorHandler("Please login to access this resource", 401));
+  //   }
 
-    const decodedData = jwt.verify(token, process.env.JWT_SECRET);
+  // const decodedData = jwt.verify(token, process.env.JWT_SECRET);
 
-    req.user = await User.findById(decodedData.id);
-    next();
-})
+  // req.user = await User.findById(decodedData.id);
+  next();
+});
 
 exports.authorizeRole = (...roles) => {
-    return (req, res, next) => {
-        if (!roles.includes(req.user.role)) {
-            return next(new ErrorHandler(
-                "Acess Denied, Only Admin can access", 403
-            ))
-            // 403 means server denied
-        }
-        next();
+  return (req, res, next) => {
+    if (!roles.includes(req.user.role)) {
+      return next(new ErrorHandler("Acess Denied, Only Admin can access", 403));
+      // 403 means server denied
     }
-}
+    next();
+  };
+};
